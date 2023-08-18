50b7e77e8bbaa27043205399472d9f868e862496
Yifan Tian <yifant@Yifans-iMac.local>
2023-08-17 22:13:51 -0400

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
+
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
+    <>
     <Component {...pageProps} />
+    <Analytics />
+    </>
   );
 };
 
